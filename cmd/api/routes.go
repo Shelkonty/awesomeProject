59cf083dadb8baae5957ddf62197e8cc41850f2a
Magzhan Seilbek <hdfghdfud@gmai;.com>
2022-12-23 23:35:15 +0600

    GoLandProject

---
+++ cmd/api/routes.go
@@ -0,0 +1,21 @@
+package main
+
+// я хочу оставлять комментарии чтобы не забыть где что и чтобы не забыть зачем мне это нужно надеюсь вы поймете меня
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+func (app *application) routes() *httprouter.Router {
+	// Initialize a new httprouter router instance.
+	router := httprouter.New()
+	// Register the relevant methods, URL patterns and handler functions for our
+	// endpoints using the HandlerFunc() method. Note that http.MethodGet and
+	// http.MethodPost are constants which equate to the strings "GET" and "POST"
+	// respectively.
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
+	// Return the httprouter instance.
+	return router
+}
