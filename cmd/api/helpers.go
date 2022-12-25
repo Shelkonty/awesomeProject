ff63aa6f77ac55406b0d072ec261ecf28b6e1de7
hdfgh <hdfghdfud@gmail.com>
2022-12-25 14:08:15 +0600

    awesome

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -8,8 +8,6 @@ import (
 	"strconv"
 )
 
-// Retrieve the "id" URL parameter from the current request context, then convert it to
-// an integer and return it. If the operation isn't successful, return 0 and an error.
 func (app *application) readIDParam(r *http.Request) (int64, error) {
 	params := httprouter.ParamsFromContext(r.Context())
 	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
@@ -19,13 +17,15 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	return id, nil
 }
 
-func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+type envelop map[string]any
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelop, headers http.Header) error {
 	// Encode the data to JSON, returning the error if there was one.
-	js, err := json.Marshal(data)
+	js, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		return err
 	}
-	// Append a newline to make it easier to view in terminal applications.
+
 	js = append(js, '\n')
 	// At this point, we know that we won't encounter any more errors before writing the
 	// response, so it's safe to add any headers that we want to include. We loop
