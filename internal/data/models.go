37fe04cb376c2db6928bd52a3df50a8e5b991a05
hdfgh <hdfghdfud@gmail.com>
2023-01-22 21:46:34 +0600

    awesome

--- internal/data/models.go
+++ internal/data/models.go
@@ -3,31 +3,29 @@ package data
 import (
 	"database/sql"
 	"errors"
-	"github.com/lib/pq"
 )
-
-func (m MovieModel) Insert(movie *Movie) error {
-
-	query := `
-INSERT INTO movies (title, year, runtime, genres)
-VALUES ($1, $2, $3, $4)
-RETURNING id, created_at, version`
-
-	args := []any{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
-
-	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
-}
 
 var (
 	ErrRecordNotFound = errors.New("record not found")
 )
 
 type Models struct {
-	Movies MovieModel
+	Movies interface {
+		Insert(movie *Movie) error
+		Get(id int64) (*Movie, error)
+		Update(movie *Movie) error
+		Delete(id int64) error
+	}
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Movies: MovieModel{DB: db},
+	}
+}
+
+func NewMockModels() Models {
+	return Models{
+		Movies: MockMovieModel{},
 	}
 }
