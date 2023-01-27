fb731c7d490ad6a99331cb1a9cb8a2f4391d7cce
hdfgh <hdfghdfud@gmail.com>
2023-01-27 16:07:27 +0600

    awesome

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -96,11 +96,12 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 		}
 		return
 	}
+
 	var input struct {
-		Title   string       `json:"title"`
-		Year    int32        `json:"year"`
-		Runtime data.Runtime `json:"runtime"`
-		Genres  []string     `json:"genres"`
+		Title   *string       `json:"title"`
+		Year    *int32        `json:"year"`
+		Runtime *data.Runtime `json:"runtime"`
+		Genres  []string      `json:"genres"`
 	}
 
 	err = app.readJSON(w, r, &input)
@@ -109,26 +110,22 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 		app.badRequestResponse(w, r, err)
 		return
 	}
-	movie.Title = input.Title
-	movie.Year = input.Year
-	movie.Runtime = input.Runtime
-	movie.Genres = input.Genres
 
-	//if input.Title != nil {
-	//	movie.Title = *input.Title
-	//}
+	if input.Title != nil {
+		movie.Title = *input.Title
+	}
 
-	//if input.Year != nil {
-	//	movie.Year = *input.Year
-	//}
-	//
-	//if input.Runtime != nil {
-	//	movie.Runtime = *input.Runtime
-	//}
-	//
-	//if input.Genres != nil {
-	//	movie.Genres = input.Genres
-	//}
+	if input.Year != nil {
+		movie.Year = *input.Year
+	}
+
+	if input.Runtime != nil {
+		movie.Runtime = *input.Runtime
+	}
+
+	if input.Genres != nil {
+		movie.Genres = input.Genres
+	}
 
 	v := validator.New()
 
@@ -139,22 +136,148 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 
 	err = app.models.Movies.Update(movie)
 	if err != nil {
-		app.serverErrorResponse(w, r, err)
+		switch {
+		case errors.Is(err, data.ErrEditConflict):
+			app.editConflictResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
 		return
 	}
-
-	//if err != nil {
-	//	switch {
-	//	case errors.Is(err, data.ErrEditConflict):
-	//		app.editConflictResponse(w, r)
-	//	default:
-	//		app.serverErrorResponse(w, r, err)
-	//	}
-	//}
-
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
 
+}
+
+//func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Request) {
+//	id, err := app.readIDParam(r)
+//	if err != nil {
+//		app.notFoundResponse(w, r)
+//		return
+//	}
+//
+//	movie, err := app.models.Movies.Get(id)
+//	if err != nil {
+//		switch {
+//		case errors.Is(err, data.ErrRecordNotFound):
+//			app.notFoundResponse(w, r)
+//		default:
+//			app.serverErrorResponse(w, r, err)
+//		}
+//		return
+//	}
+//
+//	var input struct {
+//		Title   *string       `json:"title"`
+//		Year    *int32        `json:"year"`
+//		Runtime *data.Runtime `json:"runtime"`
+//		Genres  []string      `json:"genres"`
+//	}
+//
+//	err = app.readJSON(w, r, &input)
+//	if err != nil {
+//		app.badRequestResponse(w, r, err)
+//		return
+//	}
+//
+//	if input.Title != nil {
+//		movie.Title = *input.Title
+//	}
+//
+//	if input.Year != nil {
+//		movie.Year = *input.Year
+//	}
+//
+//	if input.Runtime != nil {
+//		movie.Runtime = *input.Runtime
+//	}
+//
+//	if input.Genres != nil {
+//		movie.Genres = input.Genres
+//	}
+//
+//	v := validator.New()
+//	if data.ValidateMovie(v, movie); !v.Valid() {
+//		app.failedValidationResponse(w, r, v.Errors)
+//		return
+//	}
+//
+//	err = app.models.Movies.Update(movie)
+//	if err != nil {
+//		switch {
+//		case errors.Is(err, data.ErrEditConflict):
+//			app.editConflictResponse(w, r)
+//		default:
+//			app.serverErrorResponse(w, r, err)
+//		}
+//		return
+//	}
+//	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+//	if err != nil {
+//		app.serverErrorResponse(w, r, err)
+//	}
+//}
+
+func (app *application) deleteMovieHandler(w http.ResponseWriter, r *http.Request) {
+	// Extract the movie ID from the URL.
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+	// Delete the movie from the database, sending a 404 Not Found response to the
+	// client if there isn't a matching record.
+	err = app.models.Movies.Delete(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+	// Return a 200 OK status code along with a success message.
+	err = app.writeJSON(w, http.StatusOK, envelope{"message": "movie successfully deleted"}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
+func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request) {
+	// To keep things consistent with our other handlers, we'll define an input struct
+	// to hold the expected values from the request query string.
+	var input struct {
+		Title    string
+		Genres   []string
+		Page     int
+		PageSize int
+		Sort     string
+	}
+	// Initialize a new Validator instance.
+	v := validator.New()
+	// Call r.URL.Query() to get the url.Values map containing the query string data.
+	qs := r.URL.Query()
+	// Use our helpers to extract the title and genres query string values, falling back
+	// to defaults of an empty string and an empty slice respectively if they are not
+	// provided by the client.
+	input.Title = app.readString(qs, "title", "")
+	input.Genres = app.readCSV(qs, "genres", []string{})
+	// Get the page and page_size query string values as integers. Notice that we set
+	// the default page value to 1 and default page_size to 20, and that we pass the
+	// validator instance as the final argument here.
+	input.Page = app.readInt(qs, "page", 1, v)
+	input.PageSize = app.readInt(qs, "page_size", 20, v)
+	// Extract the sort query string value, falling back to "id" if it is not provided
+	// by the client (which will imply a ascending sort on movie ID).
+	input.Sort = app.readString(qs, "sort", "id")
+	// Check the Validator instance for any errors and use the failedValidationResponse()
+	// helper to send the client a response if necessary.
+	if !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+	// Dump the contents of the input struct in a HTTP response.
+	fmt.Fprintf(w, "%+v\n", input)
 }
--- internal/data/movies.go
+++ internal/data/movies.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"awesomeProject/internal/validator"
+	"context"
 	"database/sql"
 	"errors"
 	"github.com/lib/pq"
@@ -33,15 +34,17 @@ func ValidateMovie(v *validator.Validator, movie *Movie) {
 }
 
 func (m MovieModel) Insert(movie *Movie) error {
-
 	query := `
 INSERT INTO movies (title, year, runtime, genres)
 VALUES ($1, $2, $3, $4)
 RETURNING id, created_at, version`
-
 	args := []any{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
-
-	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
+	// Create a context with a 3-second timeout.
+	// Create a context with a 3-second timeout.
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	// Use QueryRowContext() and pass the context as the first argument.
+	return m.DB.QueryRowContext(ctx, query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
 }
 
 type MockMovieModel struct{}
@@ -67,30 +70,20 @@ type MovieModel struct {
 	DB *sql.DB
 }
 
-//func (m MovieModel) Insert(movie *Movie) error {
-//	return nil
-//}
-
 func (m MovieModel) Get(id int64) (*Movie, error) {
-	// The PostgreSQL bigserial type that we're using for the movie ID starts
-	// auto-incrementing at 1 by default, so we know that no movies will have ID values
-	// less than that. To avoid making an unnecessary database call, we take a shortcut
-	// and return an ErrRecordNotFound error straight away.
 	if id < 1 {
 		return nil, ErrRecordNotFound
 	}
-	// Define the SQL query for retrieving the movie data.
+	// Remove the pg_sleep(10) clause.
 	query := `
 SELECT id, created_at, title, year, runtime, genres, version
 FROM movies
 WHERE id = $1`
-	// Declare a Movie struct to hold the data returned by the query.
 	var movie Movie
-	// Execute the query using the QueryRow() method, passing in the provided id value
-	// as a placeholder parameter, and scan the response data into the fields of the
-	// Movie struct. Importantly, notice that we need to convert the scan target for the
-	// genres column using the pq.Array() adapter function again.
-	err := m.DB.QueryRow(query, id).Scan(
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	// Remove &[]byte{} from the first Scan() destination.
+	err := m.DB.QueryRowContext(ctx, query, id).Scan(
 		&movie.ID,
 		&movie.CreatedAt,
 		&movie.Title,
@@ -99,9 +92,6 @@ WHERE id = $1`
 		pq.Array(&movie.Genres),
 		&movie.Version,
 	)
-	// Handle any errors. If there was no matching movie found, Scan() will return
-	// a sql.ErrNoRows error. We check for this and return our custom ErrRecordNotFound
-	// error instead.
 	if err != nil {
 		switch {
 		case errors.Is(err, sql.ErrNoRows):
@@ -110,31 +100,60 @@ WHERE id = $1`
 			return nil, err
 		}
 	}
-	// Otherwise, return a pointer to the Movie struct.
 	return &movie, nil
 }
 
 func (m MovieModel) Update(movie *Movie) error {
-	// Declare the SQL query for updating the record and returning the new version
-	// number.
 	query := `
 UPDATE movies
 SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
-WHERE id = $5
+WHERE id = $5 AND version = $6
 RETURNING version`
-	// Create an args slice containing the values for the placeholder parameters.
 	args := []any{
 		movie.Title,
 		movie.Year,
 		movie.Runtime,
 		pq.Array(movie.Genres),
 		movie.ID,
+		movie.Version,
 	}
-	// Use the QueryRow() method to execute the query, passing in the args slice as a
-	// variadic parameter and scanning the new version value into the movie struct.
-	return m.DB.QueryRow(query, args...).Scan(&movie.Version)
+	// Create a context with a 3-second timeout.
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	// Use QueryRowContext() and pass the context as the first argument.
+	// Use QueryRowContext() and pass the context as the first argument.
+	err := m.DB.QueryRowContext(ctx, query, args...).Scan(&movie.Version)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrEditConflict
+		default:
+			return err
+		}
+	}
+	return nil
 }
-
 func (m MovieModel) Delete(id int64) error {
+	if id < 1 {
+		return ErrRecordNotFound
+	}
+	query := `
+DELETE FROM movies
+WHERE id = $1`
+	// Create a context with a 3-second timeout.
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	// Use ExecContext() and pass the context as the first argument.
+	result, err := m.DB.ExecContext(ctx, query, id)
+	if err != nil {
+		return err
+	}
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rowsAffected == 0 {
+		return ErrRecordNotFound
+	}
 	return nil
 }
